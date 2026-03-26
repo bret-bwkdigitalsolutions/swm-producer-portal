397a64f3b488a21570b8d3ae080ca6970f4a13c4
Bret Kramer <bretwkramer@gmail.com>
2026-03-26 14:54:36 -0500

    feat: initial commit

---
+++ src/types/next-auth.d.ts
@@ -0,0 +1,22 @@
+import "next-auth";
+
+declare module "next-auth" {
+  interface Session {
+    user: {
+      id: string;
+      name: string;
+      email: string;
+      image?: string;
+      role: string;
+      hasDistributionAccess: boolean;
+    };
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    id: string;
+    role: string;
+    hasDistributionAccess: boolean;
+  }
+}
